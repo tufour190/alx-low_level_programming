ff0ac015e41310c1d02b76f339c8223866d31661
tufour190 <tufourdaniel@gmail.com>
2023-09-08 18:04:51 -0700

    This code prints numbers below 10 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints numbers  below 10
+ * Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
