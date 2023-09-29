36e1fbb17293ce9d1b51f239a3cfbf0805e3f815
tufour190 <tufourdaniel@gmail.com>
2023-09-29 06:29:05 -0700

    Checks whether a number is even or odd

---
+++ even_odd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int number;
+
+    if (number % 2 == 0) 
+        printf("The number is Even");
+    
+    else 
+        printf("The number is Odd");
+    
+    return 0;
+}
