2e83d93423074bea65d8f54a7ffeba3699a5eed2
tufour190 <tufourdaniel@gmail.com>
2023-09-29 09:32:33 -0700

    Creating a static library

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memset - this function fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to be copied
+ * @n: number of times b will be copied
+ * Return: a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+This directory contain codes for practising how to create static libraries
